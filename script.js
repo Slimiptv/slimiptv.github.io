337d943f05a3e555e4c9de91b6fa8afacb8238f8
Slimiptv <manoelpedrosena85@gmail.com>
2025-11-26 23:38:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+  const play = document.querySelector('.play-btn');
+  play && play.addEventListener('click', function(){
+    window.open('https://wa.me/5591992462980', '_blank');
+  });
+});
